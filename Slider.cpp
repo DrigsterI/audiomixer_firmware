15337f17471fc6970bd07438c520b6337b7942d9
Mihhail Zolotarjov <135606944+maska12271@users.noreply.github.com>
2025-12-10 17:36:06 +0200

    Refactor Slider class to use 'this' pointer

--- Slider.cpp
+++ Slider.cpp
@@ -4,14 +4,16 @@
 float kp = 1;       // Proportional gain
 float ki = 0.3;       // Integral gain
 float kd = 0.000015;       // Derivative gain
-int lastError = 0;
-int integralError = 0;
 
 Slider::Slider(int in1, int in2, int enb, int pos_pot) {
   this->in1 = in1;
   this->in2 = in2;
   this->enb = enb;
   this->pos_pot = pos_pot;
+  this->lastError = 0;
+  this->integralError = 0;
+  this->currentPosition = 0;
+  this->targetPosition = 0;
 
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -19,29 +21,28 @@ Slider::Slider(int in1, int in2, int enb, int pos_pot) {
   pinMode(pos_pot, INPUT);
 }
 
-int currentPosition = 0;
-int targetPosition = 0;
 
 void Slider::setTarget(int newTargetPosition){
-  targetPosition = newTargetPosition;
+  this->targetPosition = newTargetPosition;
+  Serial.print(this->targetPosition);
 }
 
 void Slider::setVolume(int newVolume) {
-  int pos = map(newVolume, 0, 100, 0, 1023);
+  int pos = map(newVolume, 0, 100, 0, 4095);
   setTarget(pos);
 }
 
 int Slider::getVolume(){
-  int volume = map(targetPosition, 0, 1023, 0, 100);
+  int volume = map(this->targetPosition, 0, 4095, 0, 100);
   return volume;
 }
 
 void Slider::tick() {
   int pos = analogRead(pos_pot);
-  if (abs(currentPosition - pos) >= 5){
-    currentPosition = pos;
+  if (abs(this->currentPosition - pos) >= 5){
+    this->currentPosition = pos;
   }
-  int error = targetPosition - currentPosition;
+  int error = this->targetPosition - this->currentPosition;
   
   // Deadband to stop near target
   if (abs(error) < 20) {
@@ -50,14 +51,14 @@ void Slider::tick() {
   }
   
   // PID calculation
-  integralError += error;
-  integralError = constrain(integralError, -255, 255);
-  int derivative = error - lastError;
+  this->integralError += error;
+  this->integralError = constrain(this->integralError, -255, 255);
+  int derivative = error - this->lastError;
   
-  int pwmOutput = (int)(kp * error + ki * integralError + kd * derivative);
+  int pwmOutput = (int)(kp * error + ki * this->integralError + kd * derivative);
   pwmOutput = constrain(pwmOutput, -255, 255);
   
-  lastError = error;
+  this->lastError = error;
   
   // Apply minimum PWM threshold (adjust 90 if needd)
   if (pwmOutput > 0 && pwmOutput < 90) pwmOutput = 90;
@@ -81,5 +82,5 @@ void Slider::stop() {
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   analogWrite(enb, 0);
-  integralError = 0;
+  this->integralError = 0;
 }
